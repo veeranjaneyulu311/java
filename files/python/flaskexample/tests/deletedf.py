a1a6bf128a45eba2603a73356380ad36fee52a37
veeranjaneyulu <veeranjaneyulu.n@dru.care>
2019-08-12 18:10:40 +0530

    backup

--- files/python/flaskexample/tests/deletedf.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-import usaddress
-
-
-print(usaddress.parse('Robie House, 5757 South Woodlawn Avenue, Chicago, IL 60637'))
-print(usaddress.tag('Robie House, 5757 South Woodlawn Avenue, Chicago, IL 60637'))
--- files/python/mysite/login/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class User(models.Model):
-    user_name = models.CharField(max_length=200)
-    pwd = models.CharField(max_length=10)
